215381a14d69844c5eb348f88300056f9f4a8934
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-23 18:40:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,23 @@
  def fizzbuzz(i)
 
-  if i % 15 == 0
-    "FizzBuzz"
- elsif i % 5 == 0
-    "Buzz"
- elsif i % 3 == 0
-    "Fizz"
+ #  if i % 15 == 0
+ #    "FizzBuzz"
+ # elsif i % 5 == 0
+ #    "Buzz"
+ # elsif i % 3 == 0
+ #    "Fizz"
+ # end
+
+ case i
+ when i % 15 == 0
+   "FizzBuzz"
+ when i % 5 == 0
+   "Buzz"
+ when i % 3 == 0
+   "Fizz"
+ else
+   nil
  end
+
 
 end
