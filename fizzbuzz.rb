96e650122ca40349228a37fa12aa50c22d0364c0
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-23 18:44:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(i) #method accepts one argument
-  if i % 15 == 0
+  if i % 15 == 0 #method called and passed num divisible by 3 & 5, so 15
     "FizzBuzz"
-  elsif i % 5 == 0
+  elsif i % 5 == 0 #method called and passed num divisible by 5
     "Buzz"
   elsif i % 3 == 0 #method called and passed num divisible by 3
     "Fizz"
